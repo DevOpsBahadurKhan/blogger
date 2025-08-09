7c85dbba852ea0e01db9bcdba827ae1fd81b4b32
Bahadur <bk209203@gmail.com>
2025-08-09 18:52:27 +0530

    working

--- src/controllers/permission.controller.js
+++ src/controllers/permission.controller.js
@@ -6,19 +6,7 @@ import logger from '../utils/logger.js';
 
 export const createPermission = async (req, res, next) => {
     try {
-        const data = {};
-        data.possession = req.body.possession;
-        data.action = req.body.action;
-        data.resource = req.body.resource
-        const userId = req.user.id;
-        if (!userId) {
-            let err = new Error('Wrong Credentials');
-            logger.error(err);
-            throw err;
-        }
-        data.role_id = +userId;
-
-        const permission = await permissionService.createSinglePermission(data);
+        const permission = await permissionService.createSinglePermission({ role_id, action, resource, possession });
         logger.info('Permission created',
             { ip: req.ip });
 
